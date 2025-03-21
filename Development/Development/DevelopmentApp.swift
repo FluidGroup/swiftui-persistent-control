900c667c4099ee16140f80fa9b07206f4370dafb
Muukii <muukii.app@gmail.com>
2025-03-21 18:10:02 +0900

    Update

---
+++ Development/Development/DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopmentApp.swift
+//  Development
+//
+//  Created by Muukii on 2025/03/21.
+//
+
+import SwiftUI
+
+@main
+struct DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Development/DevelopmentTests/DevelopmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopmentTests.swift
+//  DevelopmentTests
+//
+//  Created by Muukii on 2025/03/21.
+//
+
+import Testing
+@testable import Development
+
+struct DevelopmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
